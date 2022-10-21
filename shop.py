52290dffbb19c9e2cc9a26783539ebbcf49ed29b
Joshua <72043974+Joshuwat12@users.noreply.github.com>
2022-10-21 02:56:17 +0000

    Bonus

--- shop.py
+++ shop.py
@@ -13,6 +13,7 @@ def PrintMenu():
   print("6: Change Price of Item in Buy or Sell List")
   print("7: Quit")
 
+
 def PrintList(l):
   print("Name".ljust(20), "|", "Price".ljust(20) + "\n")
   for item in l:
@@ -24,23 +25,40 @@ def PrintList(l):
 file = input("Enter the filename of the file in question: ")
 file = open(file, 'r')
 
-
 # Setup
 buy = {}
 sell = {}
 for tran in file.readlines():
-  tran = tran[:-1].split("\t")
-  if len(tran) > 3:
-    tran[1:-1] = " ".join(tran[1:-1]) # Joins the name into one element if necessary
-  
-  tran[2] = float(tran[2][1:])
-  if tran[0] == "Buy":
-    buy[tran[1]] = tran[2]
+  isValid = True
+  tran = tran.strip().split("\t")
+  if len(tran) != 3:
+    print(tran, "must have exactly three elements: Buy/Sell, Name, and Price.")
+    isValid = False
+  elif tran[2][0] != "$":
+    print(tran[2], "must start with $.")
+    isValid = False
   else:
-    sell[tran[1]] = tran[2]
+    try:
+      tran[2] = float(tran[2][1:])
+    except:
+      print(tran[2], "is not a valid price.")
+      isValid = False
+
+  if isValid:
+    if tran[0] == "Buy":
+      if tran[1] in buy:
+        print(tran[1], "has already been added to the Buy list.")
+      else:
+        buy[tran[1]] = tran[2]
+    elif tran[0] == "Sell":
+      if tran[1] in sell:
+        print(tran[1], "has already been added to the Sell list.")
+      else:
+        sell[tran[1]] = tran[2]
+    else:
+      print(tran[0], "is not a valid list.")
 
 file.close()
-
 
 # Menu
 money = 0
@@ -54,15 +72,15 @@ while option != "7":
     PrintList(buy)
   elif option == "2":
     PrintList(sell)
-  
+
   elif option == "3":
     item = input("Enter the name of the item being bought: ")
     if item in buy:
       money -= buy[item]
-      buy.pop(item) # https://www.w3schools.com/python/ref_dictionary_pop.asp
+      buy.pop(item)  # https://www.w3schools.com/python/ref_dictionary_pop.asp
     else:
       print("Item not found")
-  
+
   elif option == "4":
     item = input("Enter the name of the item being sold: ")
     if item in sell:
@@ -70,13 +88,15 @@ while option != "7":
       sell.pop(item)
     else:
       print("Item not found")
-  
+
   elif option == "5":
     name = input("Enter the name of the item: ")
     price = input("Enter the price for the item: ")
     try:
       price = float(price)
-      l = input("Enter 'buy' or 'sell' to add it to that list, or anything else to cancel: ")
+      l = input(
+        "Enter 'buy' or 'sell' to add it to that list, or anything else to cancel: "
+      )
       if l == "buy":
         if name in buy:
           print(name, "is already in the buy list.")
@@ -91,7 +111,9 @@ while option != "7":
       print("Invalid price")
 
   elif option == "6":
-    l = input("Enter 'buy' or 'sell' to change that list, or anything else to cancel: ")
+    l = input(
+      "Enter 'buy' or 'sell' to change that list, or anything else to cancel: "
+    )
     if l in ["buy", "sell"]:
       name = input("Enter the name of the item: ")
       price = input("Enter the price for the item: ")
@@ -109,10 +131,9 @@ while option != "7":
             print(name, "is not in the sell list.")
       except:
         print("Invalid price")
-  
+
   elif option != "7":
     print("Option not recognized")
-
 
 # Output
 net = "$" + str(money)
